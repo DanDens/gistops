57c6475d0d35503623b3312db8445134bda466e4
EC2 Default User <ec2-user@ip-172-31-14-154.eu-central-1.compute.internal>
2022-10-24 13:17:43 +0000

    mv git config into setmeup dirs

--- setmeup/chrome-os-debian-11/setmeup.sh
+++ setmeup/chrome-os-debian-11/setmeup.sh
@@ -109,16 +109,19 @@ python3.7 -m pip install -r ../../gistops/requirements.txt
 #####################
 # GIT Configuration #
 #####################
-git config credential.helper store
-git config http.sslverify true
+git config --global credential.helper store
+git config --global http.sslverify true
 
 #############
 # GIT Alias #
 #############
-git config alias.lol "log --oneline --graph --decorate --all"
-git config alias.wcd "whatchanged -p --abbrev-commit --pretty=medium"
-git config alias.mtn "mergetool --no-prompt"
-git config alias.sts "status -s" 
-git config alias.acp '!acp() { git add . && git commit -m "$1" && git push ${2-origin} ; }; acp'
+git config --global core.editor "nano"
+git config --global alias.lol "log --oneline --graph --decorate --all"
+git config --global alias.wcd "whatchanged -p --abbrev-commit --pretty=medium"
+git config --global alias.mtn "mergetool --no-prompt"
+git config --global alias.sts "status -s" 
+git config --global alias.acp '!acp() { git add . && git commit -m "$1" && git push ${2-origin} ; }; acp'
+
+git config --global --list
 
 exit 0 
--- setmeup/cloud9-amazon-linux2/setmeup.sh
+++ setmeup/cloud9-amazon-linux2/setmeup.sh
@@ -125,7 +125,23 @@ then
   rm awscliv2.zip
 fi
 
+#####################
+# GIT Configuration #
+#####################
+git config --global credential.helper store
+git config --global http.sslverify true
 
+#############
+# GIT Alias #
+#############
+git config --global core.editor "nano"
+git config --global alias.lol "log --oneline --graph --decorate --all"
+git config --global alias.wcd "whatchanged -p --abbrev-commit --pretty=medium"
+git config --global alias.mtn "mergetool --no-prompt"
+git config --global alias.sts "status -s" 
+git config --global alias.acp '!acp() { git add . && git commit -m "$1" && git push ${2-origin} ; }; acp'
+
+git config --global --list
 
 
 exit 0 
