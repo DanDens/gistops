39ffaaaaa4a0f848e50dc07d5251a605b2c2e0b5
DanDens <jens.orthmann@gmail.com>
2022-10-28 21:01:00 +0000

    fix vscode debug devenv

--- src/setup-python-venv.sh
+++ src/setup-python-venv.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-python3.7 -m pip install --user --upgrade pip
-python3.7 -m pip install --user virtualenv
-python3.7 -m venv ./venv
+python3.10 -m pip install --user --upgrade pip
+python3.10 -m pip install --user virtualenv
+python3.10 -m venv ./venv
 
 source ./venv/bin/activate
 
-python3.7 -m pip install -r ./requirements.txt
-# For interactive debugging in cloud9
-python3.7 -m pip install ikp3db
+python3.10 -m pip install -r ./requirements.txt
+python3.10 -m pip install pylint
